82b81d6cee0546d7141207a7a7499ecc51d0480b
a_szczepanik <a.szczepanik@adform.com>
2022-06-01 11:18:06 +0200

    Add case class Config for parser

---
+++ src/main/scala/Config.scala
@@ -0,0 +1,1 @@
+case class Config(wordsInput: String = "", wordsOutput: String = "", multiplierInput: String = "", multiplierOutput: String = "", negativeNumInput: String = "", negativeNumOutput: String = "")
