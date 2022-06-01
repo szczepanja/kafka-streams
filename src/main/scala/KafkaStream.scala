93633f05a3e053cf3e3c4a9b6a36c8887b53fbf7
a_szczepanik <a.szczepanik@adform.com>
2022-06-01 11:31:21 +0200

    Update method main with parses configurations

--- src/main/scala/KafkaStream.scala
+++ src/main/scala/KafkaStream.scala
@@ -1,7 +1,9 @@
+import Parser.getParser
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.kstream.KStream
 import org.apache.kafka.streams.scala.serialization.Serdes
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, Topology}
+import scopt.OParser
 
 import java.util.Properties
 
@@ -13,19 +15,23 @@ object KafkaStream {
   val NUMBER_OUTPUT_TOPIC = "number-output"
 
   def main(args: Array[String]): Unit = {
+    OParser.parse(getParser, args, Config()) match {
+      case Some(config) => {
+        val bootstrapServers = sys.env.getOrElse("BOOTSTRAP_SERVERS", ":9092")
+        val appIdConfig = sys.env.getOrElse("APP_ID", "kafka-streams")
 
-    val bootstrapServers = sys.env.getOrElse("BOOTSTRAP_SERVERS", ":9092")
-    val appIdConfig = sys.env.getOrElse("APP_ID", "kafka-streams")
+        val props = new Properties()
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, appIdConfig)
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.getClass)
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.getClass)
 
-    val props = new Properties()
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, appIdConfig)
-    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.getClass)
-    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.getClass)
-
-    val topology = getTopology
-    val stream = new KafkaStreams(topology, props)
-    stream.start()
+        val topology = getTopology
+        val stream = new KafkaStreams(topology, props)
+        stream.start()
+      }
+      case None => println("Wrong configuration")
+    }
   }
 
   def prefix(negative: Int) = s"negative number: " + negative
