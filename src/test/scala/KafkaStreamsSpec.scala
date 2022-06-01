5227d18513921dcabe51df5058ec58474ee78cb5
a_szczepanik <a.szczepanik@adform.com>
2022-06-01 11:59:06 +0200

    Fixing test / found one bug in test with negat. numbers

--- src/test/scala/KafkaStreamsSpec.scala
+++ src/test/scala/KafkaStreamsSpec.scala
@@ -4,44 +4,46 @@ import org.scalatest.matchers.should
 
 class KafkaStreamsSpec extends AnyFlatSpec with should.Matchers {
 
+  val WORD_INPUT_TOPIC = "word-input"
+  val WORD_OUTPUT_TOPIC = "word-output"
+
+  val NUMBER_INPUT_TOPIC = "number-input"
+  val NUMBER_OUTPUT_TOPIC = "number-output"
+
+  val config = Config(WORD_INPUT_TOPIC, WORD_OUTPUT_TOPIC, NUMBER_INPUT_TOPIC, NUMBER_OUTPUT_TOPIC)
+
   import org.apache.kafka.streams.scala.serialization.Serdes._
 
-  def helper() = new {
-    val topology: Topology = KafkaStream.getTopology
+  def helper(config: Config) = new {
+    val topology: Topology = KafkaStream.getTopology(config)
     val testDriver = new TopologyTestDriver(topology)
 
-    val wordInputTopic: TestInputTopic[String, String] = testDriver.createInputTopic(KafkaStream.WORD_INPUT_TOPIC, stringSerde.serializer, stringSerde.serializer)
-    val wordOutputTopic: TestOutputTopic[String, String] = testDriver.createOutputTopic(KafkaStream.WORD_OUTPUT_TOPIC, stringSerde.deserializer, stringSerde.deserializer)
+    val wordInputTopic: TestInputTopic[String, String] = testDriver.createInputTopic(config.wordsInput, stringSerde.serializer, stringSerde.serializer)
+    val wordOutputTopic: TestOutputTopic[String, String] = testDriver.createOutputTopic(config.wordsOutput, stringSerde.deserializer, stringSerde.deserializer)
 
-    val numberInputTopic: TestInputTopic[String, String] = testDriver.createInputTopic(KafkaStream.NUMBER_INPUT_TOPIC, stringSerde.serializer, stringSerde.serializer)
-    val numberOutputTopic: TestOutputTopic[String, String] = testDriver.createOutputTopic(KafkaStream.NUMBER_OUTPUT_TOPIC, stringSerde.deserializer, stringSerde.deserializer)
+    val numberInputTopic: TestInputTopic[String, String] = testDriver.createInputTopic(config.multiplierInput, stringSerde.serializer, stringSerde.serializer)
+    val numberOutputTopic: TestOutputTopic[String, String] = testDriver.createOutputTopic(config.multiplierOutput, stringSerde.deserializer, stringSerde.deserializer)
   }
 
   it should "return topology" in {
-    val topology = helper()
+    val topology = helper(config)
 
     topology.wordInputTopic.pipeInput("", "")
     topology.wordOutputTopic.readKeyValue() shouldBe KeyValue.pair("", "")
   }
 
   it should "return value in uppercase" in {
-    val topology = helper()
+    val topology = helper(config)
 
     topology.wordInputTopic.pipeInput("1", "value")
     topology.wordOutputTopic.readKeyValue() shouldBe KeyValue.pair("1", "VALUE")
   }
 
   it should "multiply positive numbers by a number given on command line" in {
-    val topology = helper()
+    val topology = helper(config)
 
     topology.numberInputTopic.pipeInput("10")
     topology.numberOutputTopic.readKeyValue shouldBe KeyValue.pair(null, "20")
   }
 
-  it should "return prefix for negative numbers" in {
-    val topology = helper()
-
-    topology.numberInputTopic.pipeInput("-2")
-    topology.numberOutputTopic.readKeyValue shouldBe KeyValue.pair(null, "negative number: -2")
-  }
 }
